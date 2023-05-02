d855072b6fc8f9dfafedd0e4c7e854168f104c86
Screxy <dvbvladis@mail.ru>
2023-05-02 13:50:02 +0300

    media added

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const burger = document.querySelector(".header__burger");
 const burgerMenu = document.querySelector(".nav-list");
 
-function showMenu(e){
+function handleBurgerClick(e){
     console.log(e.target)
     burgerMenu.classList.toggle('nav-list_active')
 }
-burger.addEventListener('click',showMenu);
+burger.addEventListener('click',handleBurgerClick);
