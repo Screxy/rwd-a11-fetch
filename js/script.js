82b5fc8515dd6ca3c66fa2d0205c863f92bbfd45
Screxy <dvbvladis@mail.ru>
2023-05-02 15:05:55 +0300

    nav-list > nav

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const burger = document.querySelector(".header__burger");
-const burgerMenu = document.querySelector(".nav-list");
+const burgerMenu = document.querySelector(".nav__list");
 
 function handleBurgerClick(e){
     console.log(e.target)
-    burgerMenu.classList.toggle('nav-list_active')
+    burgerMenu.classList.toggle('nav__list_active')
 }
 burger.addEventListener('click',handleBurgerClick);
