34850a80c1fde2fd76630e4affd776d115c32f28
Screxy <dvbvladis@mail.ru>
2023-05-03 23:03:01 +0300

    added scroll lock

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 const burger = document.querySelector(".header__burger");
 const burgerMenu = document.querySelector(".nav__list");
+const body = document.querySelector("body");
 
-function handleBurgerClick(e){
-    console.log(e.target)
-    burgerMenu.classList.toggle('nav__list_active')
+function handleBurgerClick() {
+  body.classList.toggle("lock");
+  burgerMenu.classList.toggle("nav__list_active");
 }
-burger.addEventListener('click',handleBurgerClick);
+burger.addEventListener("click", handleBurgerClick);
