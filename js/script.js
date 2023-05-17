dd54a95ae46fbb782ed8f3b372ae666bbaca8727
Screxy <dvbvladis@mail.ru>
2023-05-17 22:43:30 +0300

    all tasks completed

--- js/script.js
+++ js/script.js
@@ -3,11 +3,25 @@ const burgerMenu = document.querySelector(".navigation__list");
 const body = document.querySelector("body");
 
 function handleBurgerClick(e) {
-  body.classList.toggle("lock");
-  burgerMenu.classList.toggle("navigation__list_active");
+  if (!burgerMenu.classList.contains("navigation__list_active")) {
+    openBurgerMenu();
+    return;
+  }
+  closeBurgerMenu();
+}
+
+function openBurgerMenu() {
+  body.classList.add("lock");
+  burgerMenu.classList.add("navigation__list_active");
+  burger.classList.add("header__burger_active");
+}
+function closeBurgerMenu(e) {
+  body.classList.remove("lock");
+  burgerMenu.classList.remove("navigation__list_active");
+  burger.classList.remove("header__burger_active");
 }
 burger.addEventListener("click", handleBurgerClick);
-
+burgerMenu.addEventListener("click", closeBurgerMenu);
 // Задача 1
 const ingredients = {
   bread: 10,
@@ -81,19 +95,40 @@ function combineArrays(firstArr, secondArr) {
 console.log(combineArrays([1, 2, 3], ["a", "b", "c", "d"]));
 
 // Задача 5
-function countUniqueValues(arr){
-  let uniqValues = {}
-  for(let i = 0; i<arr.length; i++){
+function countUniqueValues(arr) {
+  let uniqValues = {};
+  for (let i = 0; i < arr.length; i++) {
     if (`${arr[i]}` in uniqValues) {
       uniqValues[`${arr[i]}`] += 1;
-    }
-    else{
+    } else {
       uniqValues[`${arr[i]}`] = 1;
     }
   }
-  return uniqValues
-};
+  return uniqValues;
+}
 
 console.table(countUniqueValues([1, 2, 1, 2, 3, 4, 2, 5]));
 
-// Задача 6
+// Задача 7
+const toats = document.querySelector(".toats");
+const toatsClose = document.querySelector(".toats__close");
+const message = {
+  name: "Andrew",
+  message: "Hello, brow",
+};
+function showToats() {
+  setTimeout(() => {
+    toats.classList.add("toats_active");
+  }, 2000); // Скрыть ошибку через 3 секунды
+  const spanItem = document.createElement("span");
+  spanItem.innerHTML = `<p class="toats__title" >${message.name} </p> <p class="toats__desc">${message.message} </p>`;
+  toats.appendChild(spanItem);
+}
+function hideToats(e) {
+  if (e.target.closest(".toats__close")) {
+    toats.classList.remove("toats_active");
+    toats.classList.add("toats_animate");
+  }
+}
+document.addEventListener("DOMContentLoaded", showToats);
+toats.addEventListener("click", hideToats);
